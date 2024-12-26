ceed6a4f6964b0c6acee0e5d66fc139ce7962a0d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-26 19:11:55 +0530

    crates: commons

--- crates/commons/src/lib.rs
+++ crates/commons/src/lib.rs
@@ -9,17 +9,6 @@
  *
  */
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn bincode_config() -> bincode::config::Configuration {
+    bincode::config::standard()
 }
