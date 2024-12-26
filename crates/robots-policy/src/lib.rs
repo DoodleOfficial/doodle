a278ed64ef4143d1c741a5a80046adbc244a42a0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-26 19:35:10 +0530

    crates: robots-policy[lib]

--- crates/robots-policy/src/lib.rs
+++ crates/robots-policy/src/lib.rs
@@ -14,8 +14,44 @@ const MAX_CHAR_LIMIT_DEFAULT: usize = 512_000;
 mod parser;
 mod pattern;
 
+use crate::parser::Line;
 use itertools::Itertools;
+use pattern::Pattern;
 use std::time::Duration;
 use url::Url;
 
-use crate::parser::Line;
+#[derive(Debug, PartialEq, Eq)]
+enum Directive {
+    Allow,
+    Disallow,
+}
+
+impl Ord for Directive {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Self::Allow, Self::Disallow) => std::cmp::Ordering::Less,
+            (Self::Disallow, Self::Allow) => std::cmp::Ordering::Greater,
+            _ => std::cmp::Ordering::Equal,
+        }
+    }
+} // impl Ord for Directive
+
+impl PartialOrd for Directive {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+} // impl PartialOrd for Directive
+
+#[derive(Debug, PartialEq, Eq)]
+struct Rule {
+    pattern: Pattern,
+    directive: Directive,
+} // struct Rule
+
+impl Ord for Rule {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.pattern
+            .cmp(&other.pattern)
+            .then(self.directive.cmp(&other.directive))
+    }
+}
