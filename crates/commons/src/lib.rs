800cb19a0dad3174a697c6a0462f5194a784b047
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-26 19:11:25 +0530

    crates: commons

---
+++ crates/commons/src/lib.rs
@@ -0,0 +1,25 @@
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @brief commons
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 Doodle Developers, Krisna Pranav
+ *
+ */
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
