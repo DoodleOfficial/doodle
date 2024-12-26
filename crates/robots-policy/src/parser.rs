395450b9373943cb8040c0314b022e328e734835
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-26 19:23:01 +0530

    crates: robots-policy[parser]

--- crates/robots-policy/src/parser.rs
+++ crates/robots-policy/src/parser.rs
@@ -28,39 +28,23 @@ pub enum Line<'a> {
     Raw(()),
 } // pub enum Line<'a>
 
-pub fn parse(input: &str) -> IResult<&str, Vec<Line>> {
-    let (input, (lines, _)) = many_till(
-        alt((
-            parse_user_agent,
-            parse_allow,
-            parse_disallow,
-            parse_sitemap,
-            parse_crawl_delay,
-            parse_raw,
-        )),
-        eof,
-    )(input)?;
-
-    Ok((input, lines))
-}
-
 fn is_not_line_ending(c: char) -> bool {
     c != '\n' && c != '\r'
-}
+} // fn is_not_line_ending(c: char) -> bool
 
 fn is_not_line_ending_or_comment(c: char) -> bool {
     is_not_line_ending(c) && c != '#'
-}
+} // fn is_not-line_ending_or_comment(c: char) -> bool
 
 fn is_carriage_return(c: char) -> bool {
     c == '\r'
-}
+} // fn is_carriage_return(c: char) -> bool
 
 fn consume_newline(input: &str) -> IResult<&str, Option<&str>> {
     let (input, _) = take_while(is_carriage_return)(input)?;
     let (input, output) = opt(tag("\n"))(input)?;
     Ok((input, output))
-}
+} // fn consume_newline(input: &str) -> IResult<&str, Option<&str>>
 
 fn product(input: &str) -> IResult<&str, &str> {
     let (input, _) = alt((preceded(space0, tag(":")), space1))(input)?;
@@ -69,7 +53,7 @@ fn product(input: &str) -> IResult<&str, &str> {
     let (input, _) = consume_newline(input)?;
     let line = line.trim();
     Ok((input, line))
-}
+} // fn product(input: &str) -> IResult<&str, &str>
 
 fn parse_user_agent(input: &str) -> IResult<&str, Line> {
     let useragent = (
@@ -80,6 +64,7 @@ fn parse_user_agent(input: &str) -> IResult<&str, Line> {
         tag_no_case("user agents"),
         tag_no_case("useragents"),
     );
+
     let (input, _) = preceded(space0, alt(useragent))(input)?;
     let (input, user_agents) = product(input)?;
 
@@ -89,4 +74,84 @@ fn parse_user_agent(input: &str) -> IResult<&str, Line> {
         .collect();
 
     Ok((input, Line::UserAgent(user_agents)))
-}
+} // fn parse_user_agent(input: &str) -> IResult<&str, Line>
+
+fn parse_allow(input: &str) -> IResult<&str, Line> {
+    let allow = (
+        tag_no_case("allow"),
+        tag_no_case("alow"),
+        tag_no_case("alaw"),
+        tag_no_case("allows"),
+        tag_no_case("alows"),
+        tag_no_case("alaws"),
+    );
+
+    let (input, _) = preceded(space0, alt(allow))(input)?;
+
+    let (input, allow) = product(input)?;
+    Ok((input, Line::Allow(allow)))
+} // fn parse_allow(input: &str) -> IResult<&str, Line>
+
+fn parse_disallow(input: &str) -> IResult<&str, Line> {
+    let disallow = (
+        tag_no_case("disallow"),
+        tag_no_case("dissallow"),
+        tag_no_case("dissalow"),
+        tag_no_case("disalow"),
+        tag_no_case("diasllow"),
+        tag_no_case("disallaw"),
+        tag_no_case("disallows"),
+        tag_no_case("dissallows"),
+        tag_no_case("dissalows"),
+        tag_no_case("disalows"),
+        tag_no_case("diasllows"),
+        tag_no_case("disallaws"),
+    );
+
+    let (input, _) = preceded(space0, alt(disallow))(input)?;
+
+    let (input, disallow) = product(input)?;
+    Ok((input, Line::Disallow(disallow)))
+} // fn parse_disallow(input: &str) -> IResult<&str, Line>
+
+fn parse_sitemap(input: &str) -> IResult<&str, Line> {
+    let sitemap = (
+        tag_no_case("sitemap"),
+        tag_no_case("site map"),
+        tag_no_case("site-map"),
+        tag_no_case("site maps"),
+        tag_no_case("site-maps"),
+        tag_no_case("site-maps"),
+    );
+
+    let (input, _) = preceded(space0, alt(sitemap))(input)?;
+
+    let (input, sitemap) = product(input)?;
+    Ok((input, Line::Sitemap(sitemap)))
+} // fn parse_sitemap(input: &str) -> IResult<&str, Line>
+
+fn parse_crawl_delay(input: &str) -> IResult<&str, Line> {
+    let crawl_delay = (
+        tag_no_case("crawl-delay"),
+        tag_no_case("crawl delay"),
+        tag_no_case("crawldelay"),
+        tag_no_case("crawl delays"),
+        tag_no_case("crawl-delays"),
+        tag_no_case("crawl-delays"),
+        tag_no_case("crawldelays"),
+    );
+
+    let (input, _) = preceded(space0, alt(crawl_delay))(input)?;
+
+    let (input, crawl_delay) = product(input)?;
+
+    let crawl_delay = crawl_delay.parse().ok().filter(|&x| x >= 0.0);
+
+    Ok((input, Line::CrawlDelay(crawl_delay)))
+} // fn parse_craw_delay
+
+fn parse_raw(input: &str) -> IResult<&str, Line> {
+    let (input, _raw) = take_while(is_not_line_ending)(input)?;
+    let (input, _) = consume_newline(input)?;
+    Ok((input, Line::Raw(())))
+} // fn parse_raw
