dce0badad2e3b345efeaf4e410f716d9528a1533
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-28 16:38:20 +0530

    crates: zimba[lib]

--- crates/zimba/src/lib.rs
+++ crates/zimba/src/lib.rs
@@ -708,39 +708,3 @@ impl Iterator for DirEntryIterator<'_> {
         Some(DirEntry::from_bytes(&self.mmap[pointer..]))
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let data_path = Path::new("../../data/test.zim");
-        if !data_path.exists() {
-            return;
-        }
-
-        let zim = ZimFile::open(data_path).unwrap();
-
-        assert_eq!(zim.header.magic, 72_173_914);
-        assert_eq!(zim.header.major_version, 5);
-        assert_eq!(zim.header.minor_version, 0);
-
-        let first_article = zim
-            .dir_entries()
-            .find(|x| match x {
-                Ok(DirEntry::Content { namespace, .. }) => *namespace == 'A',
-                _ => false,
-            })
-            .unwrap()
-            .unwrap();
-
-        let url = match first_article {
-            DirEntry::Content { url, .. } => url,
-            _ => panic!(),
-        };
-
-        assert_eq!(url, "African_Americans");
-        assert_eq!(zim.dir_entries().count(), 8477);
-    }
-}
