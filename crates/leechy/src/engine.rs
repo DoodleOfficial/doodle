87440c18753f3f21a8e78aa6b528a4b423cb9d2e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-30 19:17:09 +0530

    crates: leecy[engine]

---
+++ crates/leechy/src/engine.rs
@@ -0,0 +1,52 @@
+/**
+ * @file engine.rs
+ * @author Krisna Pranav
+ * @brief engine
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 Doodle Developers, Krisna Pranav
+ *
+ */
+use kuchiki::traits::*;
+use std::sync::LazyLock;
+
+static PARSED_CONFIG: LazyLock<Config> =
+    LazyLock::new(|| toml::from_str(include_str!("config.toml")).unwrap());
+
+#[derive(serde::Deserialize, Clone)]
+pub struct Config {
+    engines: Vec<EngineConf>,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        PARSED_CONFIG.clone()
+    }
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub struct EngineConf {
+    name: String,
+    search: String,
+    xpath: String,
+}
+
+pub struct Engine {
+    conf: EngineConf,
+}
+
+impl Engine {
+    pub fn new(conf: EngineConf) -> Self {
+        Engine { conf }
+    }
+
+    pub fn by_name(name: &str) -> Option<Self> {
+        let conf = Config::new().engines.into_iter().find(|e| e.name == name)?;
+        Some(Engine::new(conf))
+    }
+
+    pub async fn search(&self, query: &str) -> Result<Vec<Url>, reqwest::Error> {
+        let encoded_query = query.replace(' ', '+');
+    }
+}
