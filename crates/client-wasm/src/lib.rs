60f0877c930bf050060c071730d17a20de353ffd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-30 19:09:16 +0530

    crates: client-wasm[lib]

--- crates/client-wasm/src/lib.rs
+++ crates/client-wasm/src/lib.rs
@@ -13,13 +13,13 @@ use wasm_bindgen::prelude::*;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Failed to serialize!")]
+    #[error("Failed to serialize")]
     Serialization(#[from] serde_wasm_bindgen::Error),
 
-    #[error("Optics: error: {0}")]
+    #[error("Optics error: {0}")]
     OpticParse(#[from] optics::Error),
 
-    #[error("Json serialization error: ${0}")]
+    #[error("Json serialization error: {0}")]
     SerdeJson(#[from] serde_json::Error),
 }
 
@@ -37,3 +37,18 @@ extern "C" {
 
 #[wasm_bindgen]
 pub struct Optic;
+
+#[wasm_bindgen]
+impl Optic {
+    #[wasm_bindgen(js_name = parsePreferenceOptic)]
+    pub fn parse_preference_optic(contents: JsValue) -> Result<JsValue, Error> {
+        let optic_contents: String = serde_wasm_bindgen::from_value(contents)?;
+        let host_rankings = optics::Optic::parse(&optic_contents)?.host_rankings;
+
+        let rankings_json = serde_json::to_string(&host_rankings)?;
+
+        console_log(&("Parsed rankings to JSON: ".to_owned() + &rankings_json));
+
+        Ok(serde_wasm_bindgen::to_value(&rankings_json)?)
+    }
+}
