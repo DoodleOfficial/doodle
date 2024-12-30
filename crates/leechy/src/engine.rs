a971f60d3088432af9d3a103fc80b0314fbe4f6b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-30 19:17:29 +0530

    crates: leechy[engine]

--- crates/leechy/src/engine.rs
+++ crates/leechy/src/engine.rs
@@ -12,7 +12,7 @@ use kuchiki::traits::*;
 use std::sync::LazyLock;
 
 static PARSED_CONFIG: LazyLock<Config> =
-    LazyLock::new(|| toml::from_str(include_str!("config.toml")).unwrap());
+    LazyLock::new(|| toml::from_str(include_str!("conf.toml")).unwrap());
 
 #[derive(serde::Deserialize, Clone)]
 pub struct Config {
@@ -47,6 +47,39 @@ impl Engine {
     }
 
     pub async fn search(&self, query: &str) -> Result<Vec<Url>, reqwest::Error> {
-        let encoded_query = query.replace(' ', '+');
+        let encoded_query = query.replace(' ', "+");
+        let url = self.conf.search.replace("{query}", &encoded_query);
+        let useragent = crate::useragent::UserAgent::random_weighted();
+
+        let client = reqwest::Client::builder()
+            .user_agent(useragent.as_str())
+            .build()?;
+
+        let body = client.get(&url).send().await?.text().await?;
+
+        Ok(kuchiki::parse_html()
+            .one(body)
+            .select_xpath(&self.conf.xpath)
+            .map(|url| {
+                let attr = url.attributes.borrow();
+                let url = attr.get("href").unwrap();
+                Url(url.to_string())
+            })
+            .collect())
+    }
+}
+
+#[derive(Debug)]
+pub struct Url(String);
+
+impl std::fmt::Display for Url {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl AsRef<str> for Url {
+    fn as_ref(&self) -> &str {
+        &self.0
     }
 }
