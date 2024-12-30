12d6b90418abcfd9a66b59cd2af6d609b3c14625
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-30 19:28:12 +0530

    crates: leechy-py

--- crates/leechy-py/src/lib.rs
+++ crates/leechy-py/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+/**
+ * @file lib.rs
+ * @author Krisna Pranav
+ * @brief lib
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 Doodle Developers, Krisna Pranav
+ *
+ */
 
-#[cfg(test)]
-mod tests {
+use pyo3::prelude::*;
+
+#[pymodule]
+mod leechy {
     use super::*;
+    use ::leechy as lchy;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[pyclass]
+    struct Engine {
+        inner: lchy::Engine,
+    }
+
+    #[pymethods]
+    impl Engine {
+        #[new]
+        #[pyo3(signature = (name = "google"))]
+        fn new(name: &str) -> PyResult<Self> {
+            match lchy::Engine
+        }
     }
 }
